72be1e54c52f89855506e8655537be39e0a9ae98
Herman C <herman.chor@gmail.com>
2016-06-14 04:40:47 +0000

    Add side drawer component and model Update action bar to allow open/close side drawer Upgrade tns core modules to 2.0.1 Add shared config and navigation files

--- nativescript-appbuilder-spike/app/utils/widgets/action-bar.js
+++ nativescript-appbuilder-spike/app/utils/widgets/action-bar.js
@@ -1,19 +1,25 @@
 'use strict';
 
-var frameModule = require('ui/frame');
+var frameModule = require('ui/frame'),
+	view = require('ui/core/view'),
+	observable = require('data/observable');
 
 function onBack() {
     // Android only
     var topmost = frameModule.topmost();
-
     topmost.goBack();
+}
+
+function toggleDrawer(args) {
+    var sideDrawer = frameModule.topmost().getViewById('sideDrawer');
+    sideDrawer.toggleDrawerState();
 }
 
 function onIndex() {
     var topmost = frameModule.topmost();
-
     topmost.navigate('navigation/navigation');
 }
 
+exports.toggleDrawer = toggleDrawer;
 exports.onBack = onBack;
 exports.onIndex = onIndex;
