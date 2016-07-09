5c2f913f853633cc7a3fc8a4a36980834d5fc901
Tejas Golwala <tg@narola.email>
2016-07-09 07:43:36 +0000

    - Created API call for filter on Clients page [Done] - Reload message stack on send message [In-Progress]

--- nativescript-appbuilder-spike/app/components/appointmentDetails/appointmentDetails.js
+++ nativescript-appbuilder-spike/app/components/appointmentDetails/appointmentDetails.js
@@ -5,13 +5,13 @@ var AppointmentDetailsViewModel = require('./appointmentDetails-view-model');
 var Observable = require('data/observable').Observable;
 var helpers = require('../../utils/widgets/helper');
 var view = require("ui/core/view");
-var observableArrayModule = require('data/observable-array').Observable;
+var observableArrayModule = require('data/observable-array').ObservableArray;
 
 var page;
 var isInit = true;
 var appointmentDetails = new AppointmentDetailsViewModel();
 var pageData = new Observable({
-    appointmentDetails: appointmentDetails,
+    appointmentDetails: new observableArrayModule(appointmentDetails),
     isLoading: true
 });
 
@@ -29,7 +29,6 @@ exports.onLoaded = function(args) {
         	pageData.set('appointmentDetails', appointmentDetails.Result);
 			helpers.togglePageLoadingIndicator(false, pageData);
 		});
-
     helpers.platformInit(page);
     if (isInit) {
         isInit = false;
@@ -37,7 +36,6 @@ exports.onLoaded = function(args) {
 }
 
 exports.sendMessage = function(args) {
-    
     if(page.getViewById("message_box").text.length > 0){
         var new_message = {};
         new_message.type = "message_user";
@@ -45,22 +43,13 @@ exports.sendMessage = function(args) {
         var now = new Date();
         new_message.created = now.toLocaleDateString()+" "+now.toTimeString();
 
-        // pageData.appointmentDetails.history.push(new_message);
-        // pageData.set('appointmentDetails', pageData.appointmentDetails.history);
-
-        appointmentDetails.Result.history.push(new_message);
-        pageData.set('appointmentDetails', appointmentDetails.Result);
-        
-        alert(appointmentDetails.Result.history.length);
-        
+        pageData.appointmentDetails.history.push(new_message);
+        pageData.set('appointmentDetails', pageData.appointmentDetails.history);
+	
+        // alert(JSON.stringify(pageData.appointmentDetails));
+        // appointmentDetails.Result.history.push(new_message);
+        // pageData.set('appointmentDetails', appointmentDetails.Result);
+        // page.bindingContext = pageData;
         page.getViewById("message_box").text = "";
-        
-        page.bindingContext = pageData;
-        appointmentDetails = new AppointmentDetailsViewModel();
-        pageData = new Observable({
-            appointmentDetails: appointmentDetails,
-            isLoading: true
-        });
     }
-    
 }
