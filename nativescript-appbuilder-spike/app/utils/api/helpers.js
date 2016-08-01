51710c6ae326a25ffa8906d031b0d73acd714a07
Herman C <herman.chor@gmail.com>
2016-08-01 02:57:03 +0000

    Hard-code a JWT so the app will authenticate at the endpoints properly.

--- nativescript-appbuilder-spike/app/utils/api/helpers.js
+++ nativescript-appbuilder-spike/app/utils/api/helpers.js
@@ -5,9 +5,8 @@ var navigation = require('../../utils/navigation');
 exports.handleResponse = function(response) {
 	if (response.status != 200) {
         alert(JSON.stringify(response.statusText));
-		console.log(JSON.stringify(response));
 
-		if (response.status === 403) {
+		if (response.status === 403 || response.status === 401) {
 			navigation.signOut();
 		}
 
