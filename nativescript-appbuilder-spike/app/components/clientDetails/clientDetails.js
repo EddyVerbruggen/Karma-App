c9b748d14f22775c6c742497be128172a1ae888b
Tejas Golwala <tg@narola.email>
2016-09-05 05:38:57 +0000

    PR Cleanup [In-Progress]

--- nativescript-appbuilder-spike/app/components/clientDetails/clientDetails.js
+++ nativescript-appbuilder-spike/app/components/clientDetails/clientDetails.js
@@ -6,7 +6,13 @@ var Observable = require('data/observable').Observable;
 var dialogs = require("ui/dialogs");
 
 var helpers = require('../../utils/widgets/helper');
-var imageCache = require('../../utils/image-cache');
+// var imageCache = require('../../utils/image-cache');
+
+var imageCacheModule = require("ui/image-cache");
+var imageSource = require("image-source");
+var fs = require("file-system");
+var image;
+var imgSouce;
 
 var page;
 var isInit = true;
@@ -27,6 +33,12 @@ exports.onLoaded = function(args) {
 	helpers.togglePageLoadingIndicator(true, pageData);
     var gotData = page.navigationContext;
 	pageData.set('pageTitle', gotData.name);
+    
+    var cache = new imageCacheModule.Cache();
+    cache.placeholder = imageSource.fromFile(fs.path.join(__dirname, "../images/placeholder/temp-client-thumb.jpg"));
+    cache.maxRequests = 5;
+
+    cache.enableDownload();
         
 	clientDetails
 		.load(gotData.id)
@@ -37,34 +49,37 @@ exports.onLoaded = function(args) {
         	pageData.set('clientDetails', clientDetails);
 			helpers.togglePageLoadingIndicator(false, pageData);
 
-        	img = imageCache.getImage("http://images.nationalgeographic.com/wpf/media-live/photos/000/576/overrides/space207-trifid-nebula_57668_600x450.jpg");
-        
-            setTimeout(function(){ 
-                alert(img);
-				pageData.set('profileThumb', img);
-            }, 10000);
+        	// img = imageCache.getImage("http://images.nationalgeographic.com/wpf/media-live/photos/000/576/overrides/space207-trifid-nebula_57668_600x450.jpg");
+        	// setTimeout(function(){ 
+        	// alert(img);
+        	// pageData.set('profileThumb', img);
+        	// }, 10000);
         
         	
-        	// //IMAGE-CACHE MODULE
-        	// var imgSouce = imageSource.ImageSource;
-        	// var url = pageData.get('clientDetails').get('Result');//.get('images');
-        	// url = "http://images.nationalgeographic.com/wpf/media-live/photos/000/576/overrides/space207-trifid-nebula_57668_600x450.jpg";//url.images[0];
-        	// var image = cache.get(url);// Try to read the image from the cache
-        	// if (image) { // If present -- use it.
-        	// imgSouce = imageSource.fromNativeSource(image);
-        	// pageData.set('profileThumb', imgSouce);
-        	// } else { //If not present -- request its download.
-        	// cache.push({
-        	// key: url,
-        	// url: url,
-        	// completed: function (image, key) {
-        	// if (url === key) {
-        	// imgSouce = imageSource.fromNativeSource(image);
-        	// }
-        	// pageData.set('profileThumb', imgSouce);
-        	// }
-        	// });
-        	// }
+        	//IMAGE-CACHE MODULE
+        	imgSouce: imageSource.ImageSource;
+        	var url = pageData.get('clientDetails').get('Result');//.get('images');
+        	url = "http://images.nationalgeographic.com/wpf/media-live/photos/000/576/overrides/space207-trifid-nebula_57668_600x450.jpg";//url.images[0];
+        	image = cache.get(url);// Try to read the image from the cache
+        	if (image) { // If present -- use it.
+        		imgSouce = imageSource.fromNativeSource(image);
+        		pageData.set('profileThumb', imgSouce);
+                alert(JSON.stringify(pageData.get('profileThumb')));
+        	} else { //If not present -- request its download.
+                cache.push({
+                    key: url,
+                    url: url,
+                    completed: function (image, key) {
+                        // alert(url === key ? 'true' : 'false');
+                        if (url === key) {
+                            // alert(JSON.stringify(imageSource.fromNativeSource(image)));
+	                        imgSouce = imageSource.fromNativeSource(image);
+                        }
+    	                pageData.set('profileThumb', imgSouce);
+                        alert(JSON.stringify(pageData.get('profileThumb')));
+                    }
+                });
+        	}
 		});
 	
     helpers.platformInit(page);
--- nativescript-appbuilder-spike/app/utils/image-cache.js
+++ nativescript-appbuilder-spike/app/utils/image-cache.js
@@ -2,10 +2,11 @@ var imageCacheModule = require("ui/image-cache");
 var imageSource = require("image-source");
 var fs = require("file-system");
 
-var imgSouce, image;
+var image;
+var imgSouce;
 
 exports.getImage = function(url) {
-    
+
     var cache = new imageCacheModule.Cache();
     cache.placeholder = imageSource.fromFile(fs.path.join(__dirname, "../images/placeholder/temp-client-thumb.jpg"));
     cache.maxRequests = 5;
@@ -24,6 +25,7 @@ exports.getImage = function(url) {
             completed: function (image, key) {
                 alert(url === key ? 'true' : 'false');
                 if (url === key) {
+                    alert(JSON.stringify(imageSource.fromNativeSource(image)));
                     imgSouce = imageSource.fromNativeSource(image);
                 }
                 return key;
